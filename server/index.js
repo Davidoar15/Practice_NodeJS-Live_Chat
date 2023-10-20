863d821cff974554fd08cf46cca8ff54091536e0
Davidoar15 <davidoar15@gmail.com>
2023-10-20 13:10:19 -0300

    add/socket.io

--- server/index.js
+++ server/index.js
@@ -1,15 +1,24 @@
 import express from "express";
 import logger from "morgan";
+import { Server } from "socket.io";
+import { createServer } from "node:http";
 
 const PORT = process.env.PORT ?? 3000;
 
 const app = express();
+const server = createServer(app);
+const io = new Server(server);
+
+io.on("connection", () => {
+    console.log("A User has Connected");
+});
+
 app.use(logger("dev"));
 
 app.get("/", (req, res) => {
     res.sendFile(process.cwd()+'/client/index.html');
 });
 
-app.listen(PORT, () => {
+server.listen(PORT, () => {
     console.log(`Server running on port: ${PORT}`);
 });
