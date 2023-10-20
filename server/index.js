cb731bd9a115e9b028b41d6e828811f2e80eaffd
Davidoar15 <davidoar15@gmail.com>
2023-10-20 13:03:19 -0300

    add/index.html

--- server/index.js
+++ server/index.js
@@ -1,11 +1,13 @@
 import express from "express";
+import logger from "morgan";
 
 const PORT = process.env.PORT ?? 3000;
 
 const app = express();
+app.use(logger("dev"));
 
 app.get("/", (req, res) => {
-    res.send("<h1>This is the Chat</h1>");
+    res.sendFile(process.cwd()+'/client/index.html');
 });
 
 app.listen(PORT, () => {
