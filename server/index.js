31d5ba724bfe7904cd559ced64ef8ca30b691cdb
Davidoar15 <davidoar15@gmail.com>
2023-10-20 15:13:03 -0300

    try_to_add/turso_db_persistence_&_last_message

--- server/index.js
+++ server/index.js
@@ -2,12 +2,30 @@ import express from "express";
 import logger from "morgan";
 import { Server } from "socket.io";
 import { createServer } from "node:http";
+import dotenv from "dotenv";
+import { createClient } from "@libsql/client";
+
+dotenv.config();
 
 const PORT = process.env.PORT ?? 3000;
 
 const app = express();
 const server = createServer(app);
-const io = new Server(server);
+const io = new Server(server, {
+    connectionStateRecovery: {}
+});
+
+const db = createClient({
+    url: 'libsql://turso-url',
+    authToken: process.env.DB_TOKEN
+});
+
+await db.execute(`
+    CREATE TABLE IF NOT EXIST messages (
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        content TEXT
+    )
+`)
 
 io.on("connection", (socket) => {
     console.log("An User has Connected");
@@ -16,8 +34,21 @@ io.on("connection", (socket) => {
         console.log("An User has Disconnected");
     });
 
-    socket.on("chat message", (msg) => {
-        io.emit("chat message", msg);
+    socket.on("chat message", async (msg) => {
+        let result 
+
+        try {
+            result = await db.execute({
+                sql: 'INSERT INTO messages (content) VALUES (:msg)',
+                args: { msg }
+                
+            });
+        } catch(error) {
+            console.log(error);
+            return
+        };
+
+        io.emit("chat message", msg, result.lastInsertRowid.toString());
     });
 });
 
